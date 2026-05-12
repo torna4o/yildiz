44d56060507dfa6d6a9279f83dc3a50baed707d2
Güray Hatipoğlu <62484975+torna4o@users.noreply.github.com>
2026-05-12 13:54:12 +0300

    Update base.py

--- astrocore/io/base.py
+++ astrocore/io/base.py
@@ -1,1 +1,13 @@
+# astrocore/io/base.py 
 
+class DataSource: 
+  def load(self): 
+    """ Returns: 
+          dict with standard keys: 
+          { 
+          "t": ..., 
+          "y": ..., 
+          "meta": ... 
+          } 
+    """ 
+  raise NotImplementedError
