2c13294b6ba2a9f7a0eb1c9e83ab5ad169610053
Güray Hatipoğlu <62484975+torna4o@users.noreply.github.com>
2026-05-12 13:58:09 +0300

    Update local.py

--- astrocore/io/local.py
+++ astrocore/io/local.py
@@ -1,1 +1,24 @@
+from .base import DataSource
+import numpy as np
 
+
+class LocalCSV(DataSource):
+
+    def __init__(self, path):
+        self.path = path
+
+    def load(self):
+
+        data = np.loadtxt(
+            self.path,
+            delimiter=","
+        )
+
+        return {
+            "t": data[:, 0],
+            "y": data[:, 1],
+            "meta": {
+                "source": "local_csv",
+                "path": self.path
+            }
+        }
